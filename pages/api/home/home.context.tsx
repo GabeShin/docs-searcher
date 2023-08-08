db0cb40fc8e368c50ec65cbce2b29ba49ef62b84
Gabe <gabeshin.ts@gmail.com>
2023-08-08 21:02:11 +0200

    feat: init

--- pages/api/home/home.context.tsx
+++ pages/api/home/home.context.tsx
@@ -3,8 +3,6 @@ import { Dispatch, createContext } from 'react';
 import { ActionType } from '@/hooks/useCreateReducer';
 
 import { Conversation } from '@/types/chat';
-import { KeyValuePair } from '@/types/data';
-import { FolderType } from '@/types/folder';
 
 import { HomeInitialState } from './home.state';
 
@@ -12,14 +10,7 @@ export interface HomeContextProps {
   state: HomeInitialState;
   dispatch: Dispatch<ActionType<HomeInitialState>>;
   handleNewConversation: () => void;
-  handleCreateFolder: (name: string, type: FolderType) => void;
-  handleDeleteFolder: (folderId: string) => void;
-  handleUpdateFolder: (folderId: string, name: string) => void;
-  handleSelectConversation: (conversation: Conversation) => void;
-  handleUpdateConversation: (
-    conversation: Conversation,
-    data: KeyValuePair,
-  ) => void;
+  handleUpdateConversation: (conversation: Conversation) => void;
 }
 
 const HomeContext = createContext<HomeContextProps>(undefined!);
